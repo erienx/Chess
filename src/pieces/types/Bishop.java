5f7c71754d06a894ccdd3397f35ca001ce152595
erienx <mat21mat12mat@gmail.com>
2024-03-22 23:25:29 +0100

    Added specific movement for each piece

--- src/pieces/types/Bishop.java
+++ src/pieces/types/Bishop.java
@@ -1,5 +1,6 @@
 package pieces.types;
 
+import board.PointColRow;
 import pieces.Piece;
 import pieces.tools.PieceImagesLoader;
 import pieces.tools.PieceName;
@@ -21,6 +22,10 @@ public class Bishop extends Piece {
         if (!isMoveValidGeneral(newCol, newRow)) {
             return false;
         }
-        return true;
+        PointColRow point = getDeltaAndCheckMoveDiagonal(newCol, newRow);
+        System.out.println(point);
+
+        return point != null;
     }
+
 }
